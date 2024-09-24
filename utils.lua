354eed557b93b45522e7d5b7ef9f6b8d94570ad0
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2024-09-23 23:32:30 -0400

    Add tableMerge() in utils

--- utils.lua
+++ utils.lua
@@ -18,4 +18,20 @@ function Utils:randomFloat(min, max)
     return math.random() * (max - min) + min
 end
 
+    -- Merge two tables recursively.
+function Utils:tableMerge(t1, t2)
+    for k,v in pairs(t2) do
+        if type(v) == "table" then
+            if type(t1[k] or false) == "table" then
+                self:tableMerge(t1[k] or {}, t2[k] or {})
+            else
+                t1[k] = v
+            end
+        else
+            t1[k] = v
+        end
+    end
+    return t1
+end
+
 return Utils
